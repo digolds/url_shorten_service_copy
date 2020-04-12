0184170770ef6b113291d4604bdf3b4eaabf1b38
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-04-12 13:19:11 +0800

    add work flow & unit test

--- handlers/redirect_from.py
+++ handlers/redirect_from.py
@@ -1,17 +1,18 @@
 import boto3
+from botocore.exceptions import ClientError
 import os
 
+
 def redirect_from(event, context):
-    dynamo = boto3.resource('dynamodb').Table(os.environ['TABLE_NAME'])
-    response = dynamo.get_item(
-        Key={
-                'Id': event["pathParameters"]["id"]
-            })
-    item = response['Item']
-    real_url = item['original_url']
-    return {
-        "statusCode" : 301,
-        "headers" : {
-            "Location" : real_url
-        }
-    }
+    try:
+        dynamo = boto3.resource('dynamodb').Table(os.environ['TABLE_NAME'])
+        Id = event.get('pathParameters', {}).get('id', {})
+        if not Id:
+            return {'error_msg': 'Empty Id'}
+        response = dynamo.get_item(Key={'Id': Id})
+    except ClientError as e:
+        return {'error_msg': e.response['Error']['Message']}
+    else:
+        item = response['Item']
+        real_url = item['original_url']
+        return {'statusCode': 301, 'headers': {'Location': real_url}}
--- tests/test_redirect_from.py
+++ tests/test_redirect_from.py
@@ -3,28 +3,34 @@ import os
 from handlers import redirect_from, generate_a_shorter_url
 import boto3
 
+
 class TestRedirectFrom(unittest.TestCase):
-    
     def test_normal_status(self):
         os.environ['TABLE_NAME'] = 'urls'
         dynamo = boto3.resource('dynamodb').Table(os.environ['TABLE_NAME'])
         Id = generate_a_shorter_url.generate_id(7)
         original_url = "https://bibi.com"
-        response = dynamo.put_item(
-        Item={
+        response = dynamo.put_item(Item={
             'Id': Id,
             'original_url': original_url
-        }
-    )
-        event = {
-            'pathParameters' : {
-                "id" : Id
-            }
-        }
+        })
+        event = {'pathParameters': {"id": Id}}
         context = {}
+
         response = redirect_from.redirect_from(event, context)
         self.assertEqual(response['statusCode'], 301)
         self.assertEqual(response['headers']['Location'], original_url)
+
+    def test_invalid_credential_status(self):
+        os.environ['TABLE_NAME'] = 'urls'
+        os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAIOSFODNN7EXAMPLE'
+        os.environ[
+            'AWS_SECRET_ACCESS_KEY'] = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY'
+        response = redirect_from.redirect_from({}, {})
+        self.assertTrue('error_msg' in response)
+        del os.environ['AWS_ACCESS_KEY_ID']
+        del os.environ['AWS_SECRET_ACCESS_KEY']
+
 
 if __name__ == '__main__':
     unittest.main()
