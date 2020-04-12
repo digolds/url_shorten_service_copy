5431cd0e2fd4092712dd672d1d9f8d0d2dfe633c
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-04-12 22:48:00 +0800

    fix unittest

--- tests/test_generate_a_shorter_url.py
+++ tests/test_generate_a_shorter_url.py
@@ -1,13 +1,15 @@
 import unittest
 import os
+import json
 from handlers import generate_a_shorter_url
 import boto3
 
-class TestGenerateId(unittest.TestCase):
 
+class TestGenerateId(unittest.TestCase):
     def test_number(self):
         number = 7
-        self.assertEqual(len(generate_a_shorter_url.generate_id(number)), number)
+        self.assertEqual(len(generate_a_shorter_url.generate_id(number)),
+                         number)
 
     def test_random(self):
         number = 7
@@ -20,26 +22,20 @@ class TestGenerateId(unittest.TestCase):
         random_number_1 = generate_a_shorter_url.generate_id(number)
         self.assertIsInstance(random_number_1, str)
 
-class TestGenerateAShorterUrl(unittest.TestCase):
 
+class TestGenerateAShorterUrl(unittest.TestCase):
     def test_normal_status(self):
-        event = {
-            'body' : {
-                "original_url" : "https://baidu.com"
-            }
-        }
+        event = {'body': json.dumps({"original_url": "https://baidu.com"})}
         context = {}
         os.environ['TABLE_NAME'] = 'urls'
         id = generate_a_shorter_url.generate_a_shorter_url(event, context)
         dynamo = boto3.resource('dynamodb').Table(os.environ['TABLE_NAME'])
-        response = dynamo.get_item(
-            Key={
-                'Id': id
-            }
-        )
-        
+        response = dynamo.get_item(Key={'Id': id})
+
         item = response['Item']
-        self.assertEqual(item['original_url'], event['body']['original_url'])
+        self.assertEqual(item['original_url'],
+                         json.loads(event['body'])['original_url'])
+
 
 if __name__ == '__main__':
     unittest.main()
