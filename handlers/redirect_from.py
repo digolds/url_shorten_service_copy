092212e04f44a61eaed42428120123cf04948a3a
slz <zhengsl@cadg.cn>
2020-04-16 15:44:39 +0800

    fix unit test fail

--- handlers/redirect_from.py
+++ handlers/redirect_from.py
@@ -2,7 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
-from handlers import elastic_cache_helper
+from . import elastic_cache_helper
 
 def _generate_redirect_response(real_url):
     return {'statusCode': 301, 'headers': {'Location': real_url}}
