098d3cd65d1b7a94db5096c902e2148b11d44a85
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-04-16 22:17:40 +0800

    fix lambda package

--- handlers/redirect_from.py
+++ handlers/redirect_from.py
@@ -2,7 +2,7 @@ import boto3
 from botocore.exceptions import ClientError
 import os
 
-from . import elastic_cache_helper
+from handlers import elastic_cache_helper
 
 
 def _generate_redirect_response(real_url):
