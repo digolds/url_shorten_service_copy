c813d4c8482cfa598550133592592622e33dd388
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-04-16 19:55:40 +0800

    fix build error

--- handlers/elastic_cache_helper.py
+++ handlers/elastic_cache_helper.py
@@ -7,11 +7,12 @@ from package import pymemcache
 
 memcache_client = None
 
+
 def get_elastic_cache_client():
     global memcache_client
     if memcache_client:
         return memcache_client
-    
+
     elasticache_config_endpoint = "url-resource-cluster.yaeyym.cfg.use2.cache.amazonaws.com:11211"
     nodes = elasticache_auto_discovery.discover(elasticache_config_endpoint)
     nodes = map(lambda x: (x[1], int(x[2])), nodes)
