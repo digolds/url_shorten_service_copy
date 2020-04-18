25a3adaaa5da5bf0cad00e2e6a0adea44a4cb5b6
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-04-18 17:30:38 +0800

    unlock tests for mem cache

--- tests/test_elastic_cache_helper.py
+++ tests/test_elastic_cache_helper.py
@@ -4,18 +4,17 @@ from handlers import elastic_cache_helper
 
 class TestMemcacheClient(unittest.TestCase):
     def test_client_creation_status(self):
-        pass
         client = elastic_cache_helper.get_elastic_cache_client()
         self.assertTrue(client is not None)
-    
+
     def test_client_get_status(self):
-        pass
         client = elastic_cache_helper.get_elastic_cache_client()
         test_key = 'key'
         test_value = 'test value'
         client.set(test_key, test_value)
         v = client.get(test_key).decode('utf-8')
         self.assertTrue(v == test_value)
+
 
 if __name__ == '__main__':
     unittest.main()
