cfa7e86c0c37e87aaeea6ee9dec994c42edf414a
郑思龙 <zhengsilong@zhengsilongdeMacBook-Pro.local>
2020-04-20 21:37:39 +0800

    update config for swagger ui

--- generate_config.py
+++ generate_config.py
@@ -2,3 +2,4 @@ import os
 
 os.system('cp handlers/config_release.py handlers/config.py')
 os.system('cp views/js/config_release.min.js views/js/config.min.js')
+os.system('cp swagger-ui/config_release.min.js swagger-ui/config.min.js')
